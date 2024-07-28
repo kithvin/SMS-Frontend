a812eea8102c4af4d93d0beb4a118d29c0963ce6
kithvin <kithvinbandara1994@gmail.com>
2024-07-28 10:44:41 +0530

    Bold the navbr text

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -23,7 +23,7 @@ export default function Navbar() {
             <MenuIcon />
           </IconButton>
 
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1, fontWeight: 'bold'}}>
             Student Management System
           </Typography>
           <Button color="inherit" sx={{
